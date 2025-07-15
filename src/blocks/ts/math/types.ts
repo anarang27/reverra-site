a798557489789213bb98a3926822acf182e3805f
anarang27 <anarang27@vt.edu>
2025-07-14 20:39:10 -0500

    Added other animations

---
+++ src/blocks/ts/math/types.ts
@@ -0,0 +1,8 @@
+/*
+	Installed from github/ieedan/std
+*/
+
+export type Point = {
+  x: number;
+  y: number;
+};
---
+++ src/blocks/ts/rand.ts
@@ -0,0 +1,22 @@
+/*
+	Installed from github/ieedan/std
+*/
+
+/** Generate a random number that lies between the provided min and max.
+ *
+ * @param min
+ * @param max
+ * @returns
+ *
+ * ## Usage
+ * ```ts
+ * const num = rand(0, 10); // 0 >= num <= 10
+ * ```
+ */
+export function rand(min: number, max: number): number {
+  if (min > max) {
+    throw new Error("Max should not be greater than min!");
+  }
+
+  return Math.random() * (max - min) + min;
+}
