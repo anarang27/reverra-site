d82ddc0f9d043d27ddadddc4d505b49386421477
anarang27 <anarang27@vt.edu>
2025-07-15 19:47:11 -0500

    Added gooeynav to contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import Header from '../components/Header';
+import GooeyNav from '../components/GooeyNav/GooeyNav';
+
+const navItems = [
+  { label: "Home", href: "/" },
+  { label: "About", href: "/about" },
+  { label: "Services", href: "/services" },
+  { label: "Contact", href: "/contact" },
+];
 
 const Contact = () => (
   <div className="relative min-h-screen flex flex-col">
     <Header showLogo={false} />
+    <div className="absolute top-10 right-10 z-50">
+      <GooeyNav
+        items={navItems}
+        animationTime={400}
+        particleCount={15}
+        particleDistances={[90, 10]}
+        particleR={100}
+        timeVariance={200}
+        colors={[5, 6, 5, 6, 7, 5]}
+      />
+    </div>
     <main className="flex-grow px-8 pt-24">
       <div className="absolute top-6 left-6 z-50">
         <img
