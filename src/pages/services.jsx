9bb180da51d79201df73e2007d2765cb9f13b953
anarang27 <anarang27@vt.edu>
2025-07-16 21:12:55 -0500

    revert

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -2,27 +2,13 @@ import React from 'react';
 import Header from '../components/Header';
 
 const Services = () => (
-    <div className="relative min-h-screen flex flex-col">
-        <Header showLogo={false} />
-        <div className="absolute top-10 right-10 z-50">
-            <div className="absolute top-6 left-6 z-50">
-                <img
-                    src="/Transparent_Logo_Black.png"
-                    alt="Reverra Logo"
-                    className="h-28 sm:h-32"
-                />
-            </div>
-
-            <div className="text-center mt-16">
-                <h1 className="text-4xl md:text-5xl font-bold mb-4" style={{ fontFamily: 'Instrument Serif, serif' }}>
-                    Our Services
-                </h1>
-                <p className="text-lg text-gray-600" style={{ fontFamily: 'Newsreader, serif' }}>
-                    Coming soon...
-                </p>
-            </div>
-        </div>
-    </div>
+  <div className="relative bg-[#E1DFD9] min-h-screen w-full flex flex-col justify-start items-center pt-[12%] px-4 text-center">
+    <Header />
+    <h1 className="text-4xl md:text-5xl font-bold mb-4" style={{ fontFamily: 'Instrument Serif, serif' }}>
+      Our Services
+    </h1>
+    <p className="text-lg text-gray-600" style={{ fontFamily: 'Newsreader, serif' }}>Coming soon...</p>
+  </div>
 );
 
 export default Services;
