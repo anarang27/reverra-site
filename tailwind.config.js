9e3bc8347b105944ee48ccce28d9a36922d8bfed
anarang27 <anarang27@vt.edu>
2025-07-18 20:07:35 -0500

    tailwind.config.js '

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
-theme: {
-  extend: {
-    animation: {
-      'star-movement-bottom': 'star-movement-bottom 4s linear infinite alternate',
-      'star-movement-top': 'star-movement-top 4s linear infinite alternate',
-    },
-    keyframes: {
-      'star-movement-bottom': {
-        '0%': { transform: 'translate(0%, 0%)', opacity: '1' },
-        '100%': { transform: 'translate(-100%, 0%)', opacity: '0' },
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Add this line!
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'star-movement-bottom': 'star-movement-bottom 4s linear infinite alternate',
+        'star-movement-top': 'star-movement-top 4s linear infinite alternate',
       },
-      'star-movement-top': {
-        '0%': { transform: 'translate(0%, 0%)', opacity: '1' },
-        '100%': { transform: 'translate(100%, 0%)', opacity: '0' },
+      keyframes: {
+        'star-movement-bottom': {
+          '0%': { transform: 'translate(0%, 0%)', opacity: '1' },
+          '100%': { transform: 'translate(-100%, 0%)', opacity: '0' },
+        },
+        'star-movement-top': {
+          '0%': { transform: 'translate(0%, 0%)', opacity: '1' },
+          '100%': { transform: 'translate(100%, 0%)', opacity: '0' },
+        },
       },
     },
   },
+  plugins: [],
 }
